a58e689c5d378ed48f15df5f107783b9d6d0ba22
cristian wong <54443039+criwong88@users.noreply.github.com>
2024-06-07 08:10:42 -0500

    [Task] #1 Initial commit

---
+++ src/main/java/com/oaut/aragoapps/OautharagoappsApplication.java
@@ -0,0 +1,13 @@
+package com.oaut.aragoapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OautharagoappsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OautharagoappsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oaut/aragoapps/OautharagoappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oaut.aragoapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OautharagoappsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
